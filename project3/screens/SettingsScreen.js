569296b5ca8afc9bce5417b2ec61ff6266a9191a
fredriksvendsen <fredriksvendsen2@gmail.com>
2018-10-02 18:44:35 +0200

    contact form in progress

--- project3/screens/SettingsScreen.js
+++ project3/screens/SettingsScreen.js
@@ -12,7 +12,26 @@ import {
   Button,
   Alert
 } from 'react-native';
+import t from 'tcomb-form-native';
 
+const Form = t.form.Form;
+
+const User = t.struct({
+  name: t.String,
+  number: t.String,
+  email: t.maybe(t.String)
+});
+
+const options = {
+  fields: {
+    name: {
+      error: 'Please enter a name.'
+    },
+    number: {
+      error: 'Please enter a number.'
+    }
+  },
+};
 
   export default class SettingsScreen extends React.Component {
 
@@ -33,17 +52,39 @@ import {
     this.props.navigation.setParams({ handleCreateNewContact: this._createNewContact });
     }
 
+  // when "+" sign/create new contact listener
   _createNewContact = () => {
     alert("Coming soon.");
   }
 
+  // add contact
+  _handleSubmitContact = () => {
+    const value = this._form.getValue();
+  }
+
   render() {
     return (
-      <View>
+      <View style = {styles.container}>
         <ScrollView>
-          
+          <Form type={User}
+            ref={c => this._form = c}
+            options={options} />
+          <Button
+            title="Add to contacts"
+            onPress={this._handleSubmitContact}
+            //style = {styles.buttons}
+            color="#841584"
+          />
         </ScrollView>
       </View>
     );
   }
 }
+
+  const styles = StyleSheet.create({
+    container: {
+      justifyContent: 'center',
+      padding: 20,
+      backgroundColor: '#ffffff'
+    },
+  });
